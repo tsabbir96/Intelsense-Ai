a2fe2dc8882302876ad8f6d4f7273f58b4184de1
Sabbir Talukdar <t.sabbir96@gmail.com>
2020-08-22 20:08:56 +0600

    supports qna scrapping as well

--- daraz_scrapper/daraz_scrapper/spiders/jack_da_scrapper.py
+++ daraz_scrapper/daraz_scrapper/spiders/jack_da_scrapper.py
@@ -8,14 +8,47 @@ class jack_da_scrapper(scrapy.Spider):
         "https://my.daraz.com.bd/pdp/review/getReviewList?itemId=131778652&pageSize=200&filter=0&sort=0&pageNo="
     ]
 
+    
     def write_data(self, raw_data, filename):
         with open(filename, 'w', encoding='utf-8') as file:
             json.dump(raw_data, file, ensure_ascii=False)
         print('Data have saved in JSON format...')
 
-    def parse(self, response):
+    
+    def parse_qna(self,response):
         all_data  = json.loads(response.body)
-        item_data = all_data["model"]["items"]
+        item_data = item_data = all_data["model"]["items"]
+        qna_data, itemId = self.get_qnas(item_data)
+
+        filename = "QnA_Data/" + str(itemId) + ".json"
+
+        #write review data 
+        self.write_data(qna_data,filename)
+
+
+    def get_qnas(self,item_data):
+        qnas = []
+
+        for item in item_data:
+            itemId = item["itemId"]
+            customerName = item["customerName"]
+            question = item["question"]
+            answer = item["answer"]
+            a_qna = {
+                "customerName" : customerName,
+                "question" : question,
+                "answer" : answer
+            }
+            qnas.append(a_qna)
+
+        data_qna = {
+            "productId": itemId,
+            "qnas" : qnas
+        }
+        return data_qna, itemId
+
+    
+    def get_reviews(self, item_data):
         review = []
 
         for item in item_data:
@@ -38,10 +71,20 @@ class jack_da_scrapper(scrapy.Spider):
             "product_name" : itemTitle,
             "reviews" : review
         }
+        return data, itemId
+
+    
+    def parse(self, response):
+        all_data  = json.loads(response.body)
+        item_data = all_data["model"]["items"]
+        
+        review_data, itemId = self.get_reviews(item_data)
 
         filename = "Review_Data/" + str(itemId) + ".json"
 
         #write review data 
-        self.write_data(data,filename)
+        self.write_data(review_data,filename)
 
-        
+        qna_url = "https://my.daraz.com.bd/pdp/item/getQnAList?mainSeller=%7B%22itemId%22:%22" + str(itemId) + "%22,%22sellerId%22:%221000000%22,%22skuId%22:%221054764369%22%7D&isFromMainSeller=true&pageNo=&pageSize=200"
+
+        return scrapy.Request(qna_url,callback=self.parse_qna)
